b6fed0016a26045ab6e1b516484df9e1dc98a6be
Thanay Kumar Y R <thanaykumaryr@gmail.com>
2024-05-15 17:11:25 +0530

    removed: tensorflow in requirements

--- detection.py
+++ detection.py
@@ -12,7 +12,7 @@ def prediction(path,food_or_not_food_model,healthy_junk_indian_model,fruits_vege
   from keras.preprocessing.image import img_to_array,load_img
   from keras.applications.mobilenet_v2 import preprocess_input
   from query import query
-  
+
   ref = {0:"yes", 1: "no"}
   ref1 = {0: 'Healthy Food', 1: 'Indian Food', 2: 'Junk Food'}
   healthy_foods = {0: "apple",1: "banana",2: "beetroot",3: "bell pepper",4: "cabbage",5: "capsicum",6: "carrot",7: "cauliflower",8: "chilli pepper",9: "corn",10: "cucumber",11: "eggplant",12: "garlic",13: "ginger",14: "grapes",15: "kiwi",16: "lemon",17: "lettuce",18: "mango",19: "onion",20: "orange",21: "pear",22: "peas",23: "pineapple",24: "pomegranate",25: "potato",26: "radish",27: "spinach",28: "sweetcorn",29: "sweet potato",30: "tomato",31: "turnip",32: "watermelon"}
